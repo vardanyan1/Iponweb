1ba56558351b5551db61629695f24107f1949f3b
vardanyan666 <vardanyaninbox@gmail.com>
2023-01-30 18:33:48 +0400

    BST insert method done

--- src/homework_7/binary_search_tree.py
+++ src/homework_7/binary_search_tree.py
@@ -2,12 +2,50 @@ from my_execptions import BinarySearchTreeException
 from node import Node
 
 class BinarySearchTree:
-    def __init__(self, head: Node = None):
-        if (not isinstance(head, Node)) or head is None:
-            self.__head = head
+    def __init__(self, head: int = None):
+        if (not isinstance(head, int)) or (head is not None):
+            self.__head = Node(head)
 
     def __repr__(self):
-        return f"Binary Search Tree with head {self.__head}"
+        return f"BST with head: {self.__head.value}"
+
+    """
+    HEAD getter
+    """
+
+    @property
+    def head(self) -> Node:
+        return self.__head
 
     def insert(self, new_node: Node):
-        pass
+        if not isinstance(new_node, Node):
+            raise BinarySearchTreeException("New node must be Node class")
+        self.__insertion(current_node=self.__head, new_node=new_node)
+
+    def __insertion(self, current_node: Node, new_node: Node):
+        if current_node is None:
+            return new_node
+        else:
+            if current_node.value == new_node.value:
+                current_node.count += 1
+                return current_node
+            elif current_node.value < new_node.value:
+                current_node.right = self.__insertion(current_node.right, new_node)
+            else:
+                current_node.left = self.__insertion(current_node.left, new_node)
+        return current_node
+
+
+
+tree = BinarySearchTree(120)
+print(tree)
+tree.insert(Node(20))
+print(f"head.right: {tree.head.right}")
+tree.insert(Node(210))
+print(f"head.left: {tree.head.right}")
+tree.insert(Node(10))
+print(f"head.left: {tree.head.left.left}")
+# print(tree.head.right)
+# print(tree.head.left)
+# # tree.insert(Node(1))
+# # tree.insert(Node(8))
