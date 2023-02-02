162688d01e4270565add8b6b537034c5abd9b7cf
vardanyan666 <vardanyaninbox@gmail.com>
2023-02-02 23:49:44 +0400

    Isertion sort added

--- src/homework_7/sorting.py
+++ src/homework_7/sorting.py
@@ -47,8 +47,28 @@ class Sorting:
             result.extend(second_half[j:])
             return result
 
+    @staticmethod
+    def insertion_sort(items: List[Union[int, float]], reverse: bool = False) -> List[Union[int, float]]:
+        if not isinstance(items, List):
+            raise MySortingException("Items must be List type object")
+        if not all(isinstance(item, (int, float)) for item in items):
+            raise MySortingException("All items in the list must be either int or float.")
+        if not isinstance(reverse, bool):
+            raise MySortingException("Reverse must be Bool type object")
+
+        result = items.copy()
+
+        for i in range(1, len(result)):
+            j = i
+            while j > 0 and (result[j - 1] > result[j] if not reverse else result[j - 1] < result[j]):
+                result[j - 1], result[j] = result[j], result[j - 1]
+                j -= 1
+        return result
+
 
 items1 = [-6, 1, 2, 3, 5.5, 5.4, 4, -1, -66, -12]
 
-print(f"Sorted: {Sorting.merge_sort(items1, reverse=False)}")
-print(f"Reversed: {Sorting.merge_sort(items1, reverse=True)}")
+print(f"Merge Sorted: {Sorting.merge_sort(items1, reverse=False)}")
+print(f"Merge Reversed: {Sorting.merge_sort(items1, reverse=True)}")
+print(f"Insertion Sorted: {Sorting.insertion_sort(items1, reverse=False)}")
+print(f"Insertion Reversed: {Sorting.insertion_sort(items1, reverse=True)}")
