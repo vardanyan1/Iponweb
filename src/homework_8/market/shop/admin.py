cae5ece377859a641053a25fefb9795fe02ac084
vardanyan1 <vardanyaninbox@gmail.com>
2023-02-10 14:34:41 +0400

    Mybag admin link to customer id fixed

--- src/homework_8/market/shop/admin.py
+++ src/homework_8/market/shop/admin.py
@@ -93,7 +93,7 @@ class MyBagAdmin(admin.ModelAdmin):
 
     @admin.display(description='Customer')
     def username_ref(self, obj):
-        link = f"/admin/shop/customer/{obj.id}/change/"
+        link = f"/admin/shop/customer/{obj.customer.id}/change/"
         return format_html('<b><a href="{}">{}</a></b>', link, obj.customer.user.username)
 
     @admin.display(description='Items')
