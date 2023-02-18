e5bd86963e2f25b0091a8b9d064223f7ef37865b
vardanyan1 <vardanyaninbox@gmail.com>
2023-02-18 21:40:08 +0400

    store_category_serializer refactored

--- src/homework_9/src/market/serializers/store_category_serializer.py
+++ src/homework_9/src/market/serializers/store_category_serializer.py
@@ -1,6 +1,7 @@
-
-
-def store_category_serializer(category):
-    data = {"name": category.name, "id": category.id}
+def store_category_serializer(store_category):
+    data = {
+        "id": store_category.id,
+        "name": store_category.name
+    }
 
     return data
