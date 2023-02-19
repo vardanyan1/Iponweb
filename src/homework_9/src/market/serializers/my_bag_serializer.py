4ab894c855678ef2164b7a13a4e332542f7781e3
vardanyan1 <vardanyaninbox@gmail.com>
2023-02-19 16:34:38 +0400

    my_bag_serializer id added

--- src/homework_9/src/market/serializers/my_bag_serializer.py
+++ src/homework_9/src/market/serializers/my_bag_serializer.py
@@ -5,6 +5,7 @@ from .item_serializer import item_serializer
 
 def my_bag_serializer(bag) -> Dict:
     data = {
+        "id": bag.id,
         "customer": customer_serializer(bag.customer),
         "items": [item_serializer(item) for item in bag.items.all()],
         "total_price": str(bag.total_price)
