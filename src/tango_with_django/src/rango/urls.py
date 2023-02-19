b013b3779de71a76b7d8416e46e563a0624de584
vardanyan1 <vardanyaninbox@gmail.com>
2023-02-19 23:14:36 +0400

    homework_chapter3

--- src/tango_with_django/src/rango/urls.py
+++ src/tango_with_django/src/rango/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from rango import views
+from . import views
 
 app_name = 'rango'
 
 urlpatterns = [
+    path('about/', views.about, name='about'),
     path('', views.index, name='index'),
 ]
--- src/tango_with_django/src/rango/views.py
+++ src/tango_with_django/src/rango/views.py
@@ -3,4 +3,15 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("""
+    Rango says hey there partner!
+    <a href='/rango/about/'>About</a>
+
+    """)
+
+
+def about(request):
+    return HttpResponse("""
+    Rango says here is about page!
+    <a href='/rango/'>Index</a>
+    """)
