08a51d33c034e37c5b2aff117122f0db522fce52
vardanyan1 <vardanyaninbox@gmail.com>
2023-02-22 16:47:48 +0400

    Blog API add permissions

--- src/Django for APIs/Blog API/src/posts/views.py
+++ src/Django for APIs/Blog API/src/posts/views.py
@@ -1,14 +1,17 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly, )
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly, )
