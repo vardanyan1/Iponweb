fc75312af81f7a6283cd64ef094e273b1909adeb
vardanyan1 <vardanyaninbox@gmail.com>
2023-02-22 14:28:22 +0400

    TODO app(CORS, CRSF settings)

--- src/Django for APIs/TODO API/src/django_project/settings.py
+++ src/Django for APIs/TODO API/src/django_project/settings.py
@@ -38,12 +38,22 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Local
-    'todo.apps.TodoConfig'
+    'todo.apps.TodoConfig',
+    # 3d party
+    "rest_framework",
+    "corsheaders",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # new
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,3 +133,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = (
+    'http://localhost:3000',
+    'http://localhost:8000'
+
+)
+
+CSRF_TRUSTED_ORIGINS = ["localhost:3000"]
