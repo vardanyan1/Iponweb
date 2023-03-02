4664dfcced2710952b124f4c4c147c673ac794e6
vardanyan1 <vardanyaninbox@gmail.com>
2023-03-03 01:36:06 +0400

    Added partial_update custom method to prevent PATCH on customer and store_owner

--- src/homework_10/src/market/api/customer_api.py
+++ src/homework_10/src/market/api/customer_api.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.exceptions import MethodNotAllowed
+
 from ..shop.models.customer_model import Customer
 from ..serializers.serializers import CustomerSerializer
 
@@ -6,3 +8,6 @@ from ..serializers.serializers import CustomerSerializer
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+    def partial_update(self, request, *args, **kwargs):
+        raise MethodNotAllowed('PATCH')
