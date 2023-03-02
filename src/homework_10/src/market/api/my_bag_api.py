bbb4bc51ddc3ffacef67d56ab2fe82e0c61b17fc
vardanyan1 <vardanyaninbox@gmail.com>
2023-03-03 00:04:35 +0400

    my_bag api edited create method to calculate total_price and set buy_time to now().date() --minor bug

--- src/homework_10/src/market/api/my_bag_api.py
+++ src/homework_10/src/market/api/my_bag_api.py
@@ -5,7 +5,6 @@ from ..shop.models.customer_model import Customer
 from ..shop.models.item_model import Item
 from ..shop.models.my_bag_model import MyBag
 from ..serializers.serializers import MyBagSerializer
-from ..shop.models.purchase_model import Purchase
 
 
 class MyBagViewSet(viewsets.ModelViewSet):
@@ -20,7 +19,7 @@ class MyBagViewSet(viewsets.ModelViewSet):
         customer = Customer.objects.get(id=customer_id)
 
         total_price = sum(item.price for item in items)
-        purchase = Purchase.objects.create(customer=customer, total_price=total_price, buy_time=timezone.now().date())
+        purchase = MyBag.objects.create(customer=customer, total_price=total_price)
         purchase.items.set(items)
 
         serializer = self.get_serializer(purchase)
