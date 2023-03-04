1300e0df3f46355cbe8bf464818334d69bb93ebb
vardanyan1 <vardanyaninbox@gmail.com>
2023-03-04 12:46:48 +0400

    added celery docs link

--- src/homework_11/src/market/celery.py
+++ src/homework_11/src/market/celery.py
@@ -1,5 +1,5 @@
 import os
-from celery import Celery
+from celery import Celery  # https://realpython.com/asynchronous-tasks-with-django-and-celery/
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "market.settings")
 app = Celery("market")
