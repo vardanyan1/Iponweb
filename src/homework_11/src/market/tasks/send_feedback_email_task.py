fb6c56b56f044ff3262af76fb332a27c1db49826
vardanyan1 <vardanyaninbox@gmail.com>
2023-03-04 13:38:19 +0400

    small doc for flower

--- src/homework_11/src/market/tasks/send_feedback_email_task.py
+++ src/homework_11/src/market/tasks/send_feedback_email_task.py
@@ -1,17 +1,23 @@
 from time import sleep
 from django.core.mail import send_mail
 from ..celery import app
+from celery import shared_task
+
+"""
+To install and run flower need activate celery like: python3 -m celery -A market worker -l info  
+After that in another terminal run: celery --broker=redis://localhost:6379 flower
+"""
 
 
-@app.task()
+@shared_task()
 def send_feedback_email_task(email_address, message):
     """Sends an email when the feedback form has been submitted."""
 
     sleep(20)  # Simulate expensive operation(s) that freeze Django
     send_mail(
-       "Your Feedback",
-       f"\t{message}\n\nThank you!",
-       "support@example.com",
-       [email_address],
-       fail_silently=False,
+        "Your Feedback",
+        f"\t{message}\n\nThank you!",
+        "support@example.com",
+        [email_address],
+        fail_silently=False,
     )
