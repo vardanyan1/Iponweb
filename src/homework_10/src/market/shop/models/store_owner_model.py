a81d13c62d537bcf461f8adc22660adc642cf7a1
vardanyan1 <vardanyaninbox@gmail.com>
2023-03-05 18:42:13 +0400

    timezone changed back to timezone.now to avoid warning

--- src/homework_10/src/market/shop/models/store_owner_model.py
+++ src/homework_10/src/market/shop/models/store_owner_model.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class StoreOwner(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to=handle_uploaded_file, blank=True, null=True)
-    registered_at = models.DateField(default=timezone.now().date())
+    registered_at = models.DateField(default=timezone.now)
 
     def __str__(self):
         return self.user.username
